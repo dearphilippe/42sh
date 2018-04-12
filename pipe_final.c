feef52d3591870001db907a93fb5348433ea7cd0
Philippe Asf <assefphilippe@gmail.com>
2018-04-11 22:33:23 -0700

    maj

--- pipe_final.c
+++ pipe_final.c
@@ -6,7 +6,7 @@
 /*   By: passef <passef@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/11 17:47:44 by passef            #+#    #+#             */
-/*   Updated: 2018/04/11 17:48:16 by passef           ###   ########.fr       */
+/*   Updated: 2018/04/11 21:00:19 by passef           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,3 +14,13 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
+
+int main(int ac, char const * const *av)
+{
+	int		fd[2];
+
+	if(pipe(fd))
+		perror("pipe :");
+ 
+	return (0);
+}
