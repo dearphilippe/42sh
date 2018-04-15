fc2acf2d73d823804f0179abb125341f98073013
zedin27 <ztisneshamzeh@csumb.edu>
2018-04-15 02:38:42 -0700

    TODO

--- job_control/job_control.c
+++ job_control/job_control.c
@@ -6,7 +6,7 @@
 /*   By: ztisnes <ztisnes@student.42.us.org>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/13 23:50:39 by ztisnes           #+#    #+#             */
-/*   Updated: 2018/04/14 10:39:54 by ztisnes          ###   ########.fr       */
+/*   Updated: 2018/04/15 02:38:27 by ztisnes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,4 +17,8 @@
 ** job control functions that returns true or/and false when program is successful
 ** Create a signal with the PID once it is suspended after command is running (bg, fg)
 ** Ctrl Z for suspend and Ctrl C kills the process with SIGINT
+** Jobs: Print
+** fg: foreground
+** bg: background
+** &: echos the process
 /*
